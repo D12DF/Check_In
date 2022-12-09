2f06a2016a6544319a16aac3d0d21d6b182daff3
D12DF <120034427+D12DF@users.noreply.github.com>
2022-12-09 23:54:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ from selenium.webdriver.common.by import By
 import ddddocr
 from selenium.webdriver.support.ui import Select
 from util import *
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--no-sandbox') # 解决DevToolsActivePort文件不存在的报错
+chrome_options.add_argument('window-size=1920x1080') # 指定浏览器分辨率
+chrome_options.add_argument('--disable-gpu') # 谷歌文档提到需要加上这个属性来规避bug
+chrome_options.add_argument('--headless') # 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
 la = ['2616220080','2616220061']
 lc = ['Fjsy@150232','Fjsy@202016']
 for i in range(0,2):
