ec6953f85ea2763dfb49a70bab00802c32f4bda1
D12DF <120034427+D12DF@users.noreply.github.com>
2022-12-11 13:53:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -29,39 +29,38 @@ for i in range(0,2):
         username.send_keys(stu_number)
         print('登录中')
         while True:
-            try:
-                driver.find_element(By.XPATH,'//*[@id="platfrom1"]/a/img')  # 尝试获取填体温页面 成功即登录成功
-                print('登录成功')
-                break
-            except:
-                namess = driver.find_element(By.XPATH, '//*[@id="codeInput"]')
-                namess.clear()
-                stu_password = lc[i]
-                password = driver.find_element(By.XPATH, '//*[@id="Name"]')
-                password.send_keys(stu_password)
-                driver.save_screenshot("2.png")#获取页面截图
-                img = Image.open('2.png') ## 打开2.png文件，并赋值给img
-                region = img.crop((1091,343,1173,366))#对获取的截图进行裁剪
-                region.save('3.png')#保存裁剪后的图片
-                ocr = ddddocr.DdddOcr()#导入验证码识别
-                with open("3.png", "rb") as f:
-                    img_bytes = f.read()
-                res = ocr.classification(img_bytes)#将识别出来的验证码赋给res
-                print(res)
-                name = driver.find_element(By.XPATH,'//*[@id="codeInput"]')
-                number = res
-                name.send_keys(number)
-                driver.find_element(By.XPATH, '//*[@id="Submit"]').click()
+                
                 try:
-                    driver.find_element(By.XPATH, '//*[@id="layui-m-layer0"]/div[2]/div/div/div[2]/span').click()
+                        driver.find_element(By.XPATH,'//*[@id="platfrom1"]/a/img')  # 尝试获取填体温页面 成功即登录成功
+                        print('登录成功')
+                        break
                 except:
-                    break
+                        namess = driver.find_element(By.XPATH, '//*[@id="codeInput"]')
+                        namess.clear()
+                        stu_password = lc[i]
+                        password = driver.find_element(By.XPATH, '//*[@id="Name"]')
+                        password.send_keys(stu_password)
+                        driver.save_screenshot("2.png")#获取页面截图
+                        img = Image.open('2.png') ## 打开2.png文件，并赋值给img
+                        region = img.crop((1091,343,1173,366))#对获取的截图进行裁剪
+                        region.save('3.png')#保存裁剪后的图片
+                        ocr = ddddocr.DdddOcr()#导入验证码识别
+                        with open("3.png", "rb") as f:
+                                img_bytes = f.read()
+                        res = ocr.classification(img_bytes)#将识别出来的验证码赋给res
+                        print(res)
+                        name = driver.find_element(By.XPATH,'//*[@id="codeInput"]')
+                        number = res
+                        name.send_keys(number)
+                        driver.find_element(By.XPATH, '//*[@id="Submit"]').click()
+                        try:
+                                driver.find_element(By.XPATH, '//*[@id="layui-m-layer0"]/div[2]/div/div/div[2]/span').click()
+                        except:
+                                break
         #url = r'https://xg.fjsdxy.com/SPCP/Web/Report/Index'
         #driver.get(url)
         #driver.find_element(By.ID,'platfrom2').click()# 选择信息采集表
-        time.sleep(50)
         driver.find_element(By.XPATH,'//*[@id="platfrom2"]').click()# 选择信息采集表
-        time.sleep(20)
         while True:
             try:
                 while True:
