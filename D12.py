2175df594b33070b08594c208781d3783a12e162
D12DF <120034427+D12DF@users.noreply.github.com>
2022-12-11 15:32:01 +0800

    Update D12.py

--- D12.py
+++ D12.py
@@ -28,28 +28,28 @@ username = driver.find_element(By.XPATH,'//*[@id="StudentId"]')
 stu_number = la
 username.send_keys(stu_number)
 print('登录中')
-try:
-    driver.find_element(By.XPATH,'//*[@id="platfrom2"]').click()
+#try:
+    #driver.find_element(By.XPATH,'//*[@id="platfrom2"]').click()
     #driver.find_element(By.XPATH,'//*[@id="platfrom1"]/a/img')  # 尝试获取填体温页面 成功即登录成功
-    print('登录成功')
-except:
-    namess = driver.find_element(By.XPATH, '//*[@id="codeInput"]')
-    namess.clear()
-    stu_password = lc
-    password = driver.find_element(By.XPATH, '//*[@id="Name"]')
-    password.send_keys(stu_password)
-    driver.save_screenshot("2.png")#获取页面截图
-    img = Image.open('2.png') ## 打开2.png文件，并赋值给img
-    region = img.crop((1091,343,1173,366))#对获取的截图进行裁剪
-    region.save('3.png')#保存裁剪后的图片
-    ocr = ddddocr.DdddOcr()#导入验证码识别
-    with open("3.png", "rb") as f:
-        img_bytes = f.read()
-    res = ocr.classification(img_bytes)#将识别出来的验证码赋给res
-    print(res)
-    name = driver.find_element(By.XPATH,'//*[@id="codeInput"]')
-    number = res
-    name.send_keys(number)
-    driver.find_element(By.XPATH, '//*[@id="Submit"]').click()
-    print('1')
+    #print('登录成功')
+#except:
+namess = driver.find_element(By.XPATH, '//*[@id="codeInput"]')
+namess.clear()
+stu_password = lc
+password = driver.find_element(By.XPATH, '//*[@id="Name"]')
+password.send_keys(stu_password)
+driver.save_screenshot("2.png")#获取页面截图
+img = Image.open('2.png') ## 打开2.png文件，并赋值给img
+region = img.crop((1091,343,1173,366))#对获取的截图进行裁剪
+region.save('3.png')#保存裁剪后的图片
+ocr = ddddocr.DdddOcr()#导入验证码识别
+with open("3.png", "rb") as f:
+    img_bytes = f.read()
+res = ocr.classification(img_bytes)#将识别出来的验证码赋给res
+print(res)
+name = driver.find_element(By.XPATH,'//*[@id="codeInput"]')
+number = res
+name.send_keys(number)
+driver.find_element(By.XPATH, '//*[@id="Submit"]').click()
+print('1')
 driver.quit()
